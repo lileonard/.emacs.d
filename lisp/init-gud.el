0cbcf557df12fd334d2e1611257e0c46ded60d0f
lileonard <411542900@qq.com>
2016-04-25 16:38:53 +0800

    gud window-size customized

--- lisp/init-gud.el
+++ lisp/init-gud.el
@@ -33,7 +33,6 @@
   (hack-gud-mode))
 ;; }}
 
-
 (defun gud-cls (&optional num)
   "clear gud screen"
   (interactive "p")
@@ -49,24 +48,41 @@
        (t (error "GUD buffer doesn't exist!"))
        ))))
 
-(eval-after-load 'gud
-  '(progn
-     (gud-def gud-kill "kill" "\C-k" "Kill the debugee")))
-
-(defun gud-kill-yes ()
-  "gud-kill and confirm with y"
-  (interactive)
-  (let ((old-window (selected-window)))
-    (save-excursion
-      (cond
-       ((buffer-live-p (get-buffer "*gud-main*"))
-        (gud-kill nil)
-        (select-window (get-buffer-window "*gud-main*"))
-        (insert "y")
-        (comint-send-input)
-        (recenter-top-bottom)
-        (select-window old-window))
-       (t (error "GUD buffer doesn't exist!"))))))
+(defadvice gdb-setup-windows (after my-setup-gdb-windows activate)
+  "Layout the window pattern for option `gdb-many-windows'."
+  (gdb-get-buffer-create 'gdb-locals-buffer)
+  (gdb-get-buffer-create 'gdb-stack-buffer)
+  (gdb-get-buffer-create 'gdb-breakpoints-buffer)
+  (set-window-dedicated-p (selected-window) nil)
+  (switch-to-buffer gud-comint-buffer)
+  (delete-other-windows)
+  (let ((win0 (selected-window))
+        (win1 (split-window nil ( / ( * (window-height) 4) 5)))
+        (win2 (split-window nil ( / (window-height) 4)))
+        (win3 (split-window-right)))
+    (gdb-set-window-buffer (gdb-locals-buffer-name) nil win3)
+    (select-window win2)
+    (set-window-buffer
+     win2
+     (if gud-last-last-frame
+         (gud-find-file (car gud-last-last-frame))
+       (if gdb-main-file
+           (gud-find-file gdb-main-file)
+         ;; Put buffer list in window if we
+         ;; can't find a source file.
+         (list-buffers-noselect))))
+    (setq gdb-source-window (selected-window))
+    (let ((win4 (split-window-right ( / (* (window-width) 2) 3))))
+      (gdb-set-window-buffer
+       (gdb-get-buffer-create 'gdb-inferior-io) nil win4))
+    (select-window win1)
+    (gdb-set-window-buffer (gdb-stack-buffer-name))
+    (let ((win5 (split-window-right)))
+      (gdb-set-window-buffer (if gdb-show-threads-by-default
+                                 (gdb-threads-buffer-name)
+                               (gdb-breakpoints-buffer-name))
+                             nil win5))
+    (select-window win0)))
 
 (global-set-key (kbd "<f10>")    'gdb)
 (global-set-key (kbd "<C-f10>")  'gud-run)
