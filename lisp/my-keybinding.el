a5208f5478204f902c3f9c859991e793f01d7ccf
lileonard <411542900@qq.com>
2016-08-05 15:50:32 +0000

    调整鼠标滚轮滚动速度为3

--- lisp/my-keybinding.el
+++ lisp/my-keybinding.el
@@ -3,7 +3,14 @@
 ;; CentOS see Win Key as super and pass-modifier as M-x
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (global-set-key (kbd "<f3>") 'goto-line)
-;;increacs and decrease text 
+
+
+(defun up-slightly () (interactive) (scroll-up 3))
+(defun down-slightly () (interactive) (scroll-down 3))
+(global-set-key [mouse-4] 'down-slightly)
+(global-set-key [mouse-5] 'up-slightly)
+
+;;increacs and decrease text
 (global-set-key (kbd "<C-mouse-4>") 'text-scale-increase)
 (global-set-key (kbd "<C-mouse-5>") 'text-scale-decrease)
 (global-set-key (kbd "<S-f3>") 'upcase-region)
